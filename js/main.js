f55d6752821a353465419f7b7a35979125375e02
Dmitry Fateev <htmlclassic@gmail.com>
2021-02-14 21:29:31 +0300

    created first version of context-menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,44 @@
+document.onclick = function() {
+    hideCustomMenu();
+};
+
+let chat = document.querySelector('.chats');
+let isOpen = false;
+
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault(); 
+}, false);
+
+chat.addEventListener('contextmenu', function (e) {
+    showCustomMenu(e);
+}, false);
+
+
+
+function showCustomMenu(e) {
+    if (isOpen) {
+        hideCustomMenu();
+    } else {
+        let menu = document.querySelector('.context-menu');
+
+        menu.style.display = "block";
+        menu.style.left = e.clientX + "px";
+        menu.style.top = e.clientY + "px";
+
+        isOpen = true;
+    }
+}
+
+function hideCustomMenu() {
+    let menu = document.querySelector('.context-menu');
+
+    menu.style.display = "none";
+    menu.style.left = "0";
+    menu.style.top = "0";
+
+    isOpen = false;
+}
+
 function adjustTextOverflow() {
     let pinnedChat = document.querySelector('.chat-block .description .pinned-chat');
     let desc = document.querySelector('.chat-block .description');
